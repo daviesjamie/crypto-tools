9878de06a376c4d0e9c006942227beef37f23d57
Jamie Davies <jamie@jamiedavies.me>
2015-04-07 14:41:57 +0100

    Distances are measured from the start! Thanks @cbaines

--- kasisky.py
+++ kasisky.py
@@ -38,7 +38,7 @@ def get_distances_between(segment, text):
     distances = []
 
     for i in range(1, len(indices)):
-        distances.append(indices[i] - (indices[i-1] + len(segment)))
+        distances.append(indices[i] - indices[i-1])
 
     return distances
 
